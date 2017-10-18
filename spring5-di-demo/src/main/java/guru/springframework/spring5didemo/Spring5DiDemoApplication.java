7ca8612210d98a48f732c4227a97b386dd87f19f
yulikexuan <yulikexuan@gmail.com>
2017-10-18 12:00:15 -0400

    Add a controller bean to ApplicationContext

--- spring5-di-demo/src/main/java/guru/springframework/spring5didemo/Spring5DiDemoApplication.java
+++ spring5-di-demo/src/main/java/guru/springframework/spring5didemo/Spring5DiDemoApplication.java
@@ -1,12 +1,20 @@
 package guru.springframework.spring5didemo;
 
+
+import guru.springframework.spring5didemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
 public class Spring5DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5DiDemoApplication.class, args);
+		ApplicationContext actx = SpringApplication.run(
+				Spring5DiDemoApplication.class, args);
+        MyController controller = (MyController) actx.getBean(
+                "myController");
+        controller.greeting();
 	}
 }
---
+++ spring5-di-demo/src/main/java/guru/springframework/spring5didemo/controllers/MyController.java
@@ -0,0 +1,16 @@
+//: guru.springframework.spring5didemo.controllers.MyController.java
+
+package guru.springframework.spring5didemo.controllers;
+
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class MyController {
+
+    public void greeting() {
+        System.out.println("Hello Spring 5 !!!");
+    }
+
+}///~
