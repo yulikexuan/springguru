2ab5826f4380a44ce1c7551c0070cfa36bf1c74c
yulikexuan <yulikexuan@gmail.com>
2017-10-18 11:27:06 -0400

    Initial commit for spring5-di-demo

---
+++ spring5-di-demo/src/main/java/guru/springframework/spring5didemo/Spring5DiDemoApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5DiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5DiDemoApplication.class, args);
+	}
+}
