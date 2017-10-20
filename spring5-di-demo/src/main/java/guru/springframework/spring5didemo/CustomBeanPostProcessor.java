9df0aa1c792b505ca672a4ae4101130f5df5e549
yulikexuan <yulikexuan@gmail.com>
2017-10-20 11:44:33 -0400

    Implemented all bean life cycle interfaces and post processors

---
+++ spring5-di-demo/src/main/java/guru/springframework/spring5didemo/CustomBeanPostProcessor.java
@@ -0,0 +1,39 @@
+//: guru.springframework.spring5didemo.CustomBeanPostProcessor.java
+
+package guru.springframework.spring5didemo;
+
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+
+    @Nullable
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName)
+            throws BeansException {
+
+        if (bean instanceof LifeCycleDemoBean) {
+            ((LifeCycleDemoBean)bean).beforeInit();
+        }
+
+        return bean;
+    }
+
+    @Nullable
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+
+        if (bean instanceof LifeCycleDemoBean) {
+            ((LifeCycleDemoBean)bean).afterInit();
+        }
+
+        return bean;
+    }
+
+}///~
