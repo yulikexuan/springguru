d88df666cd941c058bd8625a6282f77578a898fb
yulikexuan <yulikexuan@gmail.com>
2017-10-23 13:55:37 -0400

    Setup base packages for component scan in spring5-di-demo

--- spring5-di-demo/src/main/java/guru/springframework/spring5didemo/Spring5DiDemoApplication.java
+++ spring5-di-demo/src/main/java/guru/springframework/spring5didemo/Spring5DiDemoApplication.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5didemo;
 
 
+import guru.externals.services.JokeService;
 import guru.springframework.spring5didemo.controllers.ConstructorInjectedController;
 import guru.springframework.spring5didemo.controllers.MyController;
 import guru.springframework.spring5didemo.controllers.PropertyInjectedController;
@@ -8,26 +9,33 @@ import guru.springframework.spring5didemo.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"guru.externals.services",
+        "guru.springframework.spring5didemo"})
 public class Spring5DiDemoApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ApplicationContext actx = SpringApplication.run(
-				Spring5DiDemoApplication.class, args);
+        ApplicationContext actx = SpringApplication.run(
+                Spring5DiDemoApplication.class, args);
 
-		MyController controller = (MyController) actx.getBean(
+        MyController controller = (MyController) actx.getBean(
                 "myController");
 
-		// Test DI
+        // Test DI
         System.out.println(controller.greeting());
         System.out.println(actx.getBean(PropertyInjectedController.class)
-				.sayHello());
-		System.out.println(actx.getBean(SetterInjectedController.class)
-				.sayHello());
+                .sayHello());
+        System.out.println(actx.getBean(SetterInjectedController.class)
+                .sayHello());
         System.out.println(actx.getBean(ConstructorInjectedController.class)
                 .sayHello());
-	}
-}
+
+        JokeService jokeService = (JokeService) actx.getBean("jokeService");
+        System.out.println(jokeService.getJoke());
+    }
+
+}///:~
