6fcf11d528bdf45727f3aa98fee3b3ab07c69e5b
yulikexuan <yulikexuan@gmail.com>
2017-10-26 11:56:04 -0400

    di:Using spring-boot application-properties file

--- spring5-di-demo/src/main/java/guru/springframework/spring5didemo/config/PropertyConfig.java
+++ spring5-di-demo/src/main/java/guru/springframework/spring5didemo/config/PropertyConfig.java
@@ -5,37 +5,13 @@ package guru.springframework.spring5didemo.config;
 
 import guru.springframework.spring5didemo.examplebeans.FakeDataSource;
 import guru.springframework.spring5didemo.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 
-/*
- * The @PropertySource annotation, as a convenient mechanism for adding
- * property sources to the environment. This annotation is to be used in
- * conjunction with Java based configuration and the @Configuration annotation
- *
- * The Java @PropertySource annotation does not automatically register a
- * PropertySourcesPlaceholderConfigurer with Spring. Instead, the bean must be
- * explicitly defined in the configuration to get the property resolution
- * mechanism working
- *
- * Injecting a property with the @Value annotation is straightforward
- */
 @Configuration
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-    @Autowired
-    Environment env;
 
     @Value("${guru.username}")
     String user;
@@ -57,19 +33,11 @@ public class PropertyConfig {
 
     @Bean
     public FakeDataSource fakeDataSource() {
-
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(this.env.getProperty("USERNAME"));
-        fakeDataSource.setPassword(this.env.getProperty("PASSWORD"));
+        fakeDataSource.setUser(this.user);
+        fakeDataSource.setPassword(this.password);
         fakeDataSource.setUrl(this.url);
         return fakeDataSource;
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer configurer =
-                new PropertySourcesPlaceholderConfigurer();
-        return configurer;
     }
 
     @Bean
