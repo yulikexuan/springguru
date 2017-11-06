9d3040092b4d48201b368dbcfb0522af215c82bd
yulikexuan <yulikexuan@gmail.com>
2017-11-06 13:16:19 -0500

    Unit test Category.setId()

--- spring5-recipe-app/src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
+++ spring5-recipe-app/src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
@@ -1,10 +1,18 @@
+//: guru.springframework.Spring5RecipeAppApplicationTests.java
+
+
 package guru.springframework;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+/*
+ * This is an intergration test
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Spring5RecipeAppApplicationTests {
@@ -13,4 +21,4 @@ public class Spring5RecipeAppApplicationTests {
 	public void contextLoads() {
 	}
 
-}
+}///:~
