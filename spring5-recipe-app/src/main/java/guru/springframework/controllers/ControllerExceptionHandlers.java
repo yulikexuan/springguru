079ac68b5bbf531dd272074af1fbeecb17defe09
yulikexuan <yulikexuan@gmail.com>
2018-02-07 11:44:04 -0500

    recipe: make controller advice class reactive

--- spring5-recipe-app/src/main/java/guru/springframework/controllers/ControllerExceptionHandlers.java
+++ spring5-recipe-app/src/main/java/guru/springframework/controllers/ControllerExceptionHandlers.java
@@ -5,26 +5,32 @@ package guru.springframework.controllers;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandlers {
 
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(NumberFormatException.class)
-//    public ModelAndView handleNumberFormatException(Exception e) {
-//        log.error(">>>>>>> Handling NumberFormatException: " + e.getMessage());
-//        return this.handleException(e, "400error");
-//    }
-//
-//    private ModelAndView handleException(Exception e, String viewName) {
-//        ModelAndView mav = new ModelAndView();
-//        mav.setViewName(viewName);
-//        mav.addObject("exception", e);
-//
-//        return mav;
-//    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({NumberFormatException.class,
+		    WebExchangeBindException.class})
+    public String handleNumberFormatException(Exception e, Model model) {
+
+    	log.error(">>>>>>> Handling WebExchangeBindException & " +
+			    "NumberFormatException: " + e.getMessage());
+
+		model.addAttribute("exception", e);
+
+		/*
+		 * Return the template name of "400error.html"
+		 */
+		return "400error";
+    }
 
 }///~
